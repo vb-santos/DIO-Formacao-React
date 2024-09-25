f7e84c2d56733493a347b9fc0d24c4375f0a4042
victorb-s <victorb.santos15@gmail.com>
2024-09-24 21:08:44 -0300

    Alterar nome do diretório
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

---
+++ Modulo_00_Introducao/aula00-introducao/src/App.js
@@ -0,0 +1,12 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá react</h1>
+      <Button title="Entrar"/>
+    </div>
+  );
+}
+
+export default App;
--- Modulo_00_Introducao/aula00/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      Olá React
-    </div>
-  );
-}
-
-export default App;
