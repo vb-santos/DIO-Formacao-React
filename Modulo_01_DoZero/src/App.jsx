299d925c724864f90ea26b70a4ab97df502a9ad6
victorb-s <victorb.santos15@gmail.com>
2024-09-26 13:57:41 -0300

    Aula sobre 'Ciclo de Vida de componentes'
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

--- Modulo_01_DoZero/src/App.jsx
+++ Modulo_01_DoZero/src/App.jsx
@@ -1,9 +1,36 @@
+import React, { useEffect, useState } from "react";
+
 const App = () => {
+    const [usuario, setUsuario] = useState("");
+    const [count, setCount] = useState(0);
+    const [usuarios, setUsuarios] = useState(["Pablo", "José", "Manoel"]);
+
+    const handleAddUser = () => {
+        setUsuarios([...usuarios, usuario]);
+        setUsuario('');
+    }
+
+    useEffect(()=>{
+        setCount(usuarios.length);
+    },[usuarios])
+
     return (
         <div className="App">
-            <h1>Olá mundo</h1>
+            <h1>Olá DIO</h1>
+            <h3>Total: {count}</h3>
+            <div>
+                <input
+                    value={usuario}
+                    onChange={(event) => setUsuario(event.target.value)}
+                />
+                <button onClick={handleAddUser}>Adicionar</button>
+            </div>
+            <hr />
+            {usuarios.map((item) => (
+                <p>{item}</p>
+            ))}
         </div>
-    )
-}
+    );
+};
 
 export default App;
