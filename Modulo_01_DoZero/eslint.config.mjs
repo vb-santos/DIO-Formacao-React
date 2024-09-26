a05d99e617745205b50d2e423f6a9b0d23a554ac
victorb-s <victorb.santos15@gmail.com>
2024-09-26 18:45:30 -0300

    Adicionar Prettier e EsLint
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

--- Modulo_01_DoZero/eslint.config.mjs
+++ Modulo_01_DoZero/eslint.config.mjs
@@ -1,11 +1,15 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  prettierConfig,
+  { plugins: { prettier: prettierPlugin } },
+  { rules: { "prettier/prettier": "error" } },
 ];
