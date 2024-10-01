4c7deb44bc85b5279f959975762c5591fb0cd45c
victorb-s <victorb.santos15@gmail.com>
2024-10-01 12:11:01 -0300

    Adicionar aula sobre 'React CLI' e 'React-Router'
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

---
+++ Modulo_03_Navegação/aula01-reactcli/src/pages/login/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+  return (<>
+    <h1>Login</h1>
+    <Link to="/">Voltar para home</Link>
+    </>)
+}
+
+export default Login;
